49322305fea30c9fd46f183777ea53cc57bf3ef1
Scott Burnside <sburnside77@yahoo.com>
2018-08-22 09:05:26 -0500

    Initial Version

--- DataGridSQLExample/MainWindow.xaml.cs
+++ DataGridSQLExample/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Objects;
 
 namespace DataGridSQLExample
 {
@@ -20,9 +21,25 @@ namespace DataGridSQLExample
     /// </summary>
     public partial class MainWindow : Window
     {
+        AdventureWorksLT2014Entities dataEntities = new AdventureWorksLT2014Entities();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //The below line of code generates an error...commenting it out
+            //ObjectQuery<Product> products = dataEntities.Products;
+
+            var query =
+            from product in dataEntities.Products
+            where product.Color == "Red"
+            orderby product.ListPrice
+            select new { product.Name, product.Color, CategoryName = product.ProductCategory.Name, product.ListPrice };
+
+            dataGrid1.ItemsSource = query.ToList();
         }
     }
 }
